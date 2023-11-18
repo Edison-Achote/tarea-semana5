a670cbd9119a8fa7fc19b6fa9d4f4f892dcb092a
Edison Achote <erachote@utpl.edu.ec>
2023-11-18 00:59:50 -0500

    actualizacion repositorio

--- crear_entidades.py
+++ crear_entidades.py
@@ -11,21 +11,33 @@ Base = declarative_base()
 
 # Se crea la una entidad llamada Autor, que hereda
 # de Base
-class Autor(Base):
-    __tablename__ = 'autor' # El nombre de la entidad en sqlite
+class Ciudades(Base):
+    __tablename__ = 'ciudades' # El nombre de la entidad en sqlite
     # Se definen los atributos
     id = Column(Integer, primary_key=True) # este atributo es entero y
                                         # se lo considera como llave
                                         # primaria
-    nombre = Column(String) # atributo de tipo String
-    apellido = Column(String)
-    nacionalidad = Column(String)
-    edad = Column(Integer)
+    nombreciudad = Column(String) # atributo de tipo String
+    pais = Column(String)
+    continente = Column(String)
+
 
     def __str__(self):
-        return "%s %s %s %s" % (self.nombre, self.apellido, self.nacionalidad,
-        self.edad)
+        return "%s %s %s %s", (self.nombreciudad, self.pais, self.continente)
+    
+class Estadio(Base):
+    __tablename__ = 'estadio' # El nombre de la entidad en sqlite
+    # Se definen los atributos
+    id = Column(Integer, primary_key=True) # este atributo es entero y
+                                        # se lo considera como llave
+                                        # primaria
+    nombreestadio = Column(String) # atributo de tipo String
+    ciudades = Column(String)
+    direccion = Column(String)
 
+
+    def __str__(self):
+        return "%s %s %s %s",  (self.nombreestadio, self.ciudades, self.direccion)
 # Sentencia que permite crear o migrar las clases en Python al
 # gestor de base de datos, expresado en el engine.
 Base.metadata.create_all(engine)
