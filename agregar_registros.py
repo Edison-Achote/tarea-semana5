38b8b71fc0174aa9cdf1dc9a9536494d95ca0b26
Edison Achote <erachote@utpl.edu.ec>
2023-11-18 00:44:28 -0500

    tarea ejecutada

--- agregar_registros.py
+++ agregar_registros.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import sessionmaker
 # se importa la clase(s) del
 # archivo crear_entidades
-from crear_entidades import Autor
+from crear_entidades import Ciudades
+from crear_entidades import Estadio
 # se importa el engine
 from base_datos import engine
 
@@ -14,21 +15,29 @@ Session = sessionmaker(bind=engine) # Se usa el engine
 # actualizar y generar consultas a la base de datos.
 session = Session()
 
-# se crea un objetos de tipo Autor
-autor1 = Autor(nombre="José", apellido="Armijos", nacionalidad="ecuatoriana",
-        edad=40)
-autor2 = Autor(nombre="Sara", apellido="Benitez", nacionalidad="colombiana",
-        edad=20)
-autor3 = Autor(nombre="Pedro", apellido="Díaz", nacionalidad="peruana",
-        edad=35)
-autor4 = Autor(nombre="Mónica", apellido="Carrión", nacionalidad="ecuatoriana",
-        edad=25)
+# se crea un objetos de tipo Ciudad
+ciudad1 = Ciudades(nombreciudad="latacunga", pais="ecuador", continente="america")
+ciudad2 = Ciudades(nombreciudad="pujili",  pais="ecuador", continente="america")
+ciudad3 = Ciudades(nombreciudad="salcedo",  pais="ecuador", continente="america")
+ciudad4 = Ciudades(nombreciudad="ambato",  pais="ecuador", continente="america")
+
+# se crea un objetos de tipo Estadio
+estadio1 = Estadio(nombreestadio="liga", ciudades="quito", direccion="america")
+estadio2 = Estadio(nombreestadio="barcelona", ciudades="guayaqui", direccion="america")
+estadio3 = Estadio(nombreestadio="mushukruna", ciudades="ambato", direccion="america")
+estadio4 = Estadio(nombreestadio="atahulpa", ciudades="quito", direccion="america")
+
 # se agrega los objetos de tipo Autor a la sesión
 # a la espera de un commit
-session.add(autor1)
-session.add(autor2)
-session.add(autor3)
-session.add(autor4)
+session.add(ciudad1)
+session.add(ciudad2)
+session.add(ciudad3)
+session.add(ciudad4)
+
+session.add(estadio1)
+session.add(estadio2)
+session.add(estadio3)
+session.add(estadio4)
 
 # se necesita confirmar los cambios que existan en la sessió
 # se usar commit
